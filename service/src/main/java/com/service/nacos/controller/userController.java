bd05445d68680048fbe208bb42652e88745ce8aa
xiao liuyang <papermanxly@163.com>
2021-04-19 21:53:01 +0800

    nacos 配置文件获取测试提交

--- service/src/main/java/com/service/nacos/controller/userController.java
+++ service/src/main/java/com/service/nacos/controller/userController.java
@@ -4,8 +4,11 @@ package com.service.nacos.controller;
 import com.service.nacos.entity.User;
 import com.service.nacos.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("user")
@@ -13,11 +16,18 @@ public class userController {
 
     @Autowired
     UserService userservice;
+    @Value("${nacos.testParam}")
+    private String testParam;
 
 
     @RequestMapping("getUser")
     User getUser(){
         return userservice.getUser();
+    }
+
+    @PostConstruct
+    private void test(){
+        System.out.println("1231----------"+testParam);
     }
 
 }
