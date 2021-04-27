acda7fa3940a2949ff45091cc2c35758bcc228fa
xiao liuyang <papermanxly@163.com>
2021-04-27 21:27:12 +0800

    新增 - gateway模块

---
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,27 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/customerUser/getUser")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://localhost:56010"))
+                .build();
+    }
+
+}
